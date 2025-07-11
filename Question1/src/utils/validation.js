0a4471131aaf17d6a6eb3d3249048a705ee154b5
Sai Harika Agepati <128914517+Harikaroy@users.noreply.github.com>
2025-07-11 11:47:17 +0530

    Update validation.js

--- Question1/src/utils/validation.js
+++ Question1/src/utils/validation.js
@@ -1,1 +1,12 @@
+export function isValidURL(url) {
+  const pattern = new RegExp("^(https?:\\/\\/)[\\w.-]+(?:\\.[\\w\.-]+)+[\\/#?]?.*$", "i");
+  return pattern.test(url);
+}
 
+export function isValidShortcode(code) {
+  return /^[a-zA-Z0-9]{4,10}$/.test(code);
+}
+
+export function isValidMinutes(min) {
+  return Number.isInteger(min) && min > 0;
+}
