8343c4f5098f25ef355d37c1ac855d08336d599f
Sai Harika Agepati <128914517+Harikaroy@users.noreply.github.com>
2025-07-11 11:45:57 +0530

    Update logger.js

--- Question1/src/services/logger.js
+++ Question1/src/services/logger.js
@@ -1,1 +1,16 @@
+export function logEvent(type, message, metadata = {}) {
+  const logs = JSON.parse(localStorage.getItem("logs") || "[]");
+  const entry = {
+    timestamp: new Date().toISOString(),
+    type,
+    message,
+    metadata,
+  };
+  logs.push(entry);
+  localStorage.setItem("logs", JSON.stringify(logs));
+}
+
+export function getLogs() {
+  return JSON.parse(localStorage.getItem("logs") || "[]");
+}
 
