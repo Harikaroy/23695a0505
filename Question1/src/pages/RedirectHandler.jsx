17583eeaa2695edfd8846be7a0c9562b3eebd4c3
Sai Harika Agepati <128914517+Harikaroy@users.noreply.github.com>
2025-07-11 11:45:30 +0530

    Update RedirectHandler.jsx

--- Question1/src/pages/RedirectHandler.jsx
+++ Question1/src/pages/RedirectHandler.jsx
@@ -1,1 +1,22 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { findShortURL, logClick } from "../services/urlService";
+
+const RedirectHandler = () => {
+  const { shortcode } = useParams();
+
+  useEffect(() => {
+    const record = findShortURL(shortcode);
+    if (record) {
+      logClick(shortcode, document.referrer);
+      window.location.href = record.originalURL;
+    } else {
+      alert("Shortcode not found or expired");
+    }
+  }, [shortcode]);
+
+  return null;
+};
+
+export default RedirectHandler;
 
