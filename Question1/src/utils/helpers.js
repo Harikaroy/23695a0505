b0271d5b129e757a85ba391aca108d43ff3d8208
Sai Harika Agepati <128914517+Harikaroy@users.noreply.github.com>
2025-07-11 11:46:56 +0530

    Update helpers.js

--- Question1/src/utils/helpers.js
+++ Question1/src/utils/helpers.js
@@ -1,1 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
+export function generateShortcode() {
+  return uuidv4().slice(0, 6);
+}
+
+export function getExpiry(minutes) {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + minutes);
+  return now.toISOString();
+}
 
